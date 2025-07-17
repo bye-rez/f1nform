9028944ff085796a3789ddce545dcdc8f0bde6f9
bye-rez <bairez14@icloud.com>
2025-07-16 20:21:26 -0500

    Initial Commit

---
+++ f1nform/f1nformApp.swift
@@ -0,0 +1,17 @@
+//
+//  f1nformApp.swift
+//  f1nform
+//
+//  Created by Alex Bairez on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct f1nformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ f1nformTests/f1nformTests.swift
@@ -0,0 +1,17 @@
+//
+//  f1nformTests.swift
+//  f1nformTests
+//
+//  Created by Alex Bairez on 7/16/25.
+//
+
+import Testing
+@testable import f1nform
+
+struct f1nformTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
